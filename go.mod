efd6836aec068281d1a2d077146f948ce6e5d3aa
Sameer Kulkarni <sameerkulkarni164@gmail.com>
2023-10-24 12:03:28 +0530

    Connection to rabbitMQ

---
+++ go.mod
@@ -0,0 +1,5 @@
+module leaderboard
+
+go 1.21.3
+
+require github.com/streadway/amqp v1.1.0
---
+++ leaderboard.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/streadway/amqp"
+)
+
+func main() {
+	fmt.Println("Leaderboard app")
+
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+
+	defer conn.Close()
+
+	fmt.Println("Successfully connected to Rabbit MQ!!")
+}
